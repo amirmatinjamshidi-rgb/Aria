f4c2ee1034d44af87236f68f38e8320982dfa245
AmirMatin <amirmatinjamshidi@gmail.com>
2026-07-29 15:40:50 +0330

    .

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ["**/dist/**", "**/node_modules/**", "**/.turbo/**"],
+  },
+  {
+    rules: {
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { prefer: "type-imports", fixStyle: "inline-type-imports" },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/no-explicit-any": "error",
+    },
+  },
+);
